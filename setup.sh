246b5332459877c310ddc9cc203302f930a67164
Bernd Strebel <bernd.strebel@coredev.org>
2021-03-28 03:38:05 +0200

    optimized setup.sh script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
-exec ./generate_config.sh
-
+./generate_config.sh
+source .env
+$NGINX_PROXY_PATH/checkhost.sh $MAILCOW_HOSTNAME
+echo "login with [admin:moohoo] to continue configuration after startup!"
+echo -n "continue with startup? " && read dummy
+docker-compose -f docker-compose.yml up -d --build
+echo -n "show startup logs? " && read dummy
+docker-compose -f docker-compose.yml logs -f --tail 1000
