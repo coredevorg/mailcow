aebc7a6bc1d050a34949a6d2ba08e1e09e3654aa
Bernd Strebel <bernd.strebel@coredev.org>
2021-03-29 08:29:22 +0200

    nginx-proxy autodiscover support

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 ./generate_config.sh
 source .env
+DOMAIN=${MAILCOW_HOSTNAME#*.}
+echo "AUTODISCOVER=autodicover.$DOMAIN,autoconfig.$DOMAIN" >> .env
 $NGINX_PROXY_PATH/checkhost.sh $MAILCOW_HOSTNAME
 echo "login with [admin:moohoo] to continue configuration after startup!"
 echo -n "continue with startup? " && read dummy
